80a4efbe693502587f5549ac2370f5c029b033f4
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-06-05 22:49:41 -0300

    feat: add contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,37 @@
+import { useState } from "react";
 import styles from "../styles/Contact.module.css";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    if(name === '' || email === '' || message === '') {
+      alert("Preencha todos os campos")
+      return
+    }
+
+    const templateParams = {
+      from_name: name,
+      message: message,
+      email: email
+    }
+
+    emailjs.send('service_84xapi3', 'template_7jiae8q', templateParams, 'Va9st1oQUYtD-GKbX')
+    .then((response) => {
+      console.log("Email enviado", response.status, response.text)
+      setName('')
+      setEmail('')
+      setMessage('')
+    }, (err) => {
+      console.log("ERRO: ", err)
+    })
+  }
+
   return(
     <section className={styles.footer}>
       <div>
@@ -10,20 +41,47 @@ const Contact = () => {
           Nossa equipe entrará em contato o mais breve possível para atendê-lo da melhor forma.
         </p>
       </div>
-      <div className={styles.container_inputs}>
-        <div>
-          <input className={styles.input} type="text" placeholder="Nome"/>
-          <input className={styles.input} type="email" placeholder="Email"/>
+      
+      <form onSubmit={sendEmail}>
+        <div className={styles.container_inputs}>
+          <div>
+            <input 
+              className={styles.input} 
+              type="text" 
+              placeholder="Nome"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+            <input 
+              className={styles.input} 
+              type="email" 
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </div>
+          <div>
+            <input className={styles.input} type="tel" placeholder="Telefone"/>
+            <input className={styles.input} type="text" placeholder="Assunto"/>
+          </div>
+          <div>
+            <input 
+              className={styles.input_mensage} 
+              type="text" 
+              placeholder="Mensagem"
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+            />
+          </div>
+          <button 
+            className={styles.send}
+            type="submit"
+            value="Enviar"
+          >
+            Enviar
+          </button>
         </div>
-        <div>
-          <input className={styles.input} type="tel" placeholder="Telefone"/>
-          <input className={styles.input} type="text" placeholder="Assunto"/>
-        </div>
-        <div>
-          <input className={styles.input_mensage} type="text" placeholder="Mensagem "/>
-        </div>
-        <button className={styles.send}>Enviar</button>
-      </div>
+      </form>
     </section>
   )
 }
