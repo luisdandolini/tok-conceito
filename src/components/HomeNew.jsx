9cf12e14bf162a94b620b5b9cfe2cb02517ba36e
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-08-29 15:22:44 -0300

    new images

--- src/components/HomeNew.jsx
+++ src/components/HomeNew.jsx
@@ -2,16 +2,22 @@ import styles from '../styles/HomeNew.module.css';
 import Logo from "../assets/Logo.svg"
 import { useState, useEffect } from 'react';
 import { useMediaQuery } from 'react-responsive';
-import ImageFirst from "../assets/Mask1.png";
-import ImageSecond from "../assets/Mask2.png";
-import ImageThird from "../assets/Mask3.png";
-import ImageFourth from "../assets/Mask4.png";
+import Image1 from "../assets/home/Mask group-1.png";
+import Image2 from "../assets/home/Mask group-2.png";
+import Image3 from "../assets/home/Mask group-3.png";
+import Image4 from "../assets/home/Mask group-4.png";
+import Image5 from "../assets/home/Mask group-5.png";
+import Image6 from "../assets/home/Mask group-6.png";
+import Image7 from "../assets/home/Mask group-7.png";
+import Image8 from "../assets/home/Mask group-8.png";
+import Image9 from "../assets/home/Mask group-9.png";
+import Image10 from "../assets/home/Mask group.png";
 import HomeNewMobile from './mobile/HomeNewMobile';
 
 const HomeNew = () => {
   const [isMobile, setIsMobile] = useState(false);
   const isMobileQuery = useMediaQuery({ query: `(max-width: 767px)` });
-  const [images, setImages] = useState([ImageFirst, ImageSecond, ImageThird, ImageFourth]);
+  const [images, setImages] = useState([Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8, Image9, Image10]);
   const [index, setIndex] = useState(0);
   const [fade, setFade] = useState(false);
   const [scrolled, setScrolled] = useState(false);
